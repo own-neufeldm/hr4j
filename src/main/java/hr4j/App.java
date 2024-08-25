f8d84c90decc504896d284084c594a9e8ebd9a8e
Martin Neufeld <86769574+own-neufeldm@users.noreply.github.com>
2024-08-25 20:59:58 +0000

    feat: implement

--- src/main/java/hr4j/App.java
+++ src/main/java/hr4j/App.java
@@ -1,29 +1,46 @@
 package hr4j;
 
+import java.util.ArrayList;
+import java.util.List;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Help.Ansi;
+import picocli.CommandLine.Help.ColorScheme;
 
-
-// Usage: hr [OPTIONS] [TITLE] COMMAND [ARGS]...
-
-// Print horizontal rules.
-
-// Arguments:
-// [TITLE] An optional title.
-
-// Options:
-// -l, --length INTEGER Minimum character length. [default: 50]
-// -b, --border TEXT Character(s) to use for outer borders. [default: #]
-// -f, --filler TEXT Character to use for inner fillers. [default: -]
-// -p, --paragraph Prepend 'BEGIN' and 'END' before title.
-// -u, --upper Convert title to uppercase.
-// -n, --no-newline Do not print a new-line character at the end.
-// --version Show version and exit.
-// --help Show this message and exit.
-
-@Command(name = "hr4j", version = "v1.0.0")
+@Command(name = "hr4j", version = "v1.0.0", abbreviateSynopsis = true,
+        description = "%n  Print horizontal rules.", parameterListHeading = "%nArguments:%n",
+        optionListHeading = "%nOptions:%n", separator = " ", sortOptions = false)
 public class App implements Runnable {
+    @Parameters(arity = "0..1", description = "An optional title.", paramLabel = "TITLE")
+    private String title = "";
+
+    @Option(names = {"-l", "--length"},
+            description = "Minimum character length. [default: ${DEFAULT-VALUE}]",
+            paramLabel = "INT")
+    private int length = 50;
+
+    @Option(names = {"-b", "--border"},
+            description = "Character(s) to use for outer borders. [default: ${DEFAULT-VALUE}]",
+            paramLabel = "STR")
+    private String border = "//";
+
+    @Option(names = {"-f", "--filler"},
+            description = "Character to use for inner fillers. [default: ${DEFAULT-VALUE}]",
+            paramLabel = "STR")
+    private String filler = "-";
+
+    @Option(names = {"-p", "--paragraph"}, description = "Prepend 'BEGIN' and 'END' before title.")
+    private boolean asParagraph = false;
+
+    @Option(names = {"-u", "--upper"}, description = "Convert title to uppercase.")
+    private boolean toUpper = false;
+
+    @Option(names = {"-n", "--no-newline"},
+            description = "Do not print a new-line character at the end.")
+    private boolean noNewline = false;
+
     @Option(names = {"-v", "--version"}, versionHelp = true, description = "Show version and exit.")
     private boolean showVersion = false;
 
@@ -31,11 +48,43 @@ public class App implements Runnable {
     private boolean showHelp = false;
 
     public void run() {
-        throw new RuntimeException("Not implemented.");
+        List<String> titles = new ArrayList<String>();
+
+        if (title.isEmpty()) {
+            if (asParagraph) {
+                titles.add("BEGIN");
+                titles.add("END");
+            } else {
+                titles.add("");
+            }
+        } else {
+            if (toUpper) {
+                title = title.toUpperCase();
+            }
+
+            if (asParagraph) {
+                titles.add(String.format("BEGIN %s", title));
+                titles.add(String.format("END %s", title));
+            } else {
+                titles.add(title);
+            }
+        }
+
+        for (String title : titles) {
+            String horizontalRule =
+                    hr4j.Lib.getHorizontalRule(title, length, border, filler.substring(0, 1));
+
+            if (noNewline) {
+                System.out.print(horizontalRule);
+            } else {
+                System.out.println(horizontalRule);
+            }
+        }
     }
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
+        ColorScheme colorScheme = CommandLine.Help.defaultColorScheme(Ansi.OFF);
+        int exitCode = new CommandLine(new App()).setColorScheme(colorScheme).execute(args);
         System.exit(exitCode);
     }
 }
--- src/main/java/hr4j/Lib.java
+++ src/main/java/hr4j/Lib.java
@@ -1,7 +1,18 @@
 package hr4j;
 
+import java.lang.Math;
+
 public class Lib {
-    public static String getHorizontalString() {
-        return "";
+    public static String getHorizontalRule(String title, int length, String border, String filler) {
+        String reversedBorder = new StringBuilder(border).reverse().toString();
+        if (title.isEmpty()) {
+            int fillerLength = Math.max(length - 2 - 2 * border.length(), 1);
+            return String.format("%s %s %s", border, filler.repeat(fillerLength), reversedBorder);
+        }
+        int fillerLength = Math.max(length - 4 - title.length() - 2 * border.length(), 2);
+        int sideFillerLength = Math.ceilDiv(fillerLength, 2);
+        String sideFiller = filler.repeat(sideFillerLength);
+        return String.format("%s %s %s %s %s", border, sideFiller, title, sideFiller,
+                reversedBorder);
     }
 }
